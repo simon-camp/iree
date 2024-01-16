72cc9733a6bbeab678b0b651a486eb3f5a4eddb7
Simon Camphausen <simon.camphausen@iml.fraunhofer.de>
2024-01-16 14:38:21 +0000

    Use emitc.verbatim op

--- compiler/src/iree/compiler/Dialect/VM/Conversion/VMToEmitC/EmitCTypeConverter.h
+++ compiler/src/iree/compiler/Dialect/VM/Conversion/VMToEmitC/EmitCTypeConverter.h
@@ -25,6 +25,7 @@ public:
   Type convertTypeAsNonPointer(Type type) const;
   Type convertTypeAsPointer(Type type) const;
   emitc::OpaqueType convertTypeAsCType(Type type) const;
+  std::optional<std::string> convertTypeToStringLiteral(Type type) const;
 
   SetVector<Operation *> sourceMaterializations;
   mutable ModuleAnalysis analysis;
